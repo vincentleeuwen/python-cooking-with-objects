e9b1483e6dd87d8c0e5ed56e3c8a93bd70522b60
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-04-17 14:20:30 +0200

    #8

---
+++ 08/check.py
@@ -0,0 +1,8 @@
+
+class Check(object):
+
+    def __init__(self):
+        self.items = []
+
+    def add(self, item):
+        self.items.append(item)
---
+++ 08/dish.py
@@ -0,0 +1,6 @@
+
+class Dish(object):
+
+    def __init__(self, dish_name, ingredients):
+        self.name = dish_name
+        self.ingredients = ingredients
